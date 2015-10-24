2c984fc68d762cd62da04cb239ae0d3cfa072fa9
Sameer Deshpande <deshpande.sameer@gmail.com>
2015-10-24 18:11:22 +0530

    Used EXIT_* macros in source code

--- echo.c
+++ echo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -10,5 +11,5 @@ int main(int argc, char *argv[])
         printf(" %s", argv[i]);
 
     printf("\n");
-    return 0;
+    return EXIT_SUCCESS;
 }
